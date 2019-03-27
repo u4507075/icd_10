b1ca359eac28056c9fb4fe4479803adc437dd2e9
Piyapong <u4507075@hotmail.com>
2019-03-27 09:49:07 +0000

    edit

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from preprocessing.trainmodel import get_small_sample
 from preprocessing.lab import get_lab_data
 from preprocessing.lab import split_lab_data
 from preprocessing.lab import clean_lab_data
-from preprocessing.lab import onehot_lab_data
+from preprocessing.lab import tonumeric_lab_data
 
 from preprocessing.admit import save_admit_data
 from preprocessing.admit import clean_admit_data
@@ -40,8 +40,8 @@ feature = 'drug'
 
 #get_lab_data(config)
 #split_lab_data()
-clean_lab_data()
-#onehot_lab_data()
+#clean_lab_data()
+##tonumeric_lab_data()
 
 #save_admit_data(config)
 #clean_admit_data()
@@ -51,6 +51,31 @@ clean_lab_data()
 #clean_registration_data()
 #onehot_registration_data()
 
+'''
+import pandas as pd
 
-
-
+files = os.listdir('../../secret/data/lab/clean/')
+for lab in files:
+	n = lab.replace('.csv','')
+	print('#### '+n)
+	print('')
+	print('Laboratory findings of '+n+'.')
+	print('')
+	print('<details><summary>lab metadata</summary>')
+	print('<p>')
+	print('')
+	print('| Features | Types | Description |')
+	print('| :--- | :--- | :--- |')
+	print('| TXN | numeric | key identification for a patient visit |')
+	
+	p = '../../secret/data/lab/clean/'+lab
+	for df in  pd.read_csv(p, chunksize=10, index_col=0):
+		for i in df.columns:
+			if i != 'TXN' and i != 'icd10' and i != 'Unnamed: 0.1':
+				print('| '+i+' | binary |  |')
+		break
+	print('| icd10 | text | ICD-10 code (diagnosis) |')
+	print('</p>')
+	print('</details>')
+	print('')
+'''
