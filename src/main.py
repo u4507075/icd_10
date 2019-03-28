96c25f72f35d35b294f0aea91d49f71a0f2135fc
Purilarp Daoaroonkiet <billza7@gmail.com>
2019-03-28 18:11:11 +0700

    Read csv file

--- src/main.py
+++ src/main.py
@@ -1,20 +1,4 @@
-from db.database import getdata
-from db.database import cleandata
-from db.database import readdata
-from db.database import onehotdrug
-from db.database import mapdata
-from db.database import train_model
-import sys
-import os
-from pathlib import Path
-sys.path.append(str(Path(os.path.abspath('..')).parent)+'/secret')
+from radio.radio import getdata
 
-import config
-
-#getdata(config)
-#cleandata()
-#readdata()
-onehotdrug()
-#mapdata()
-#train_model()
+getdata()
 
--- src/radio/radio.py
+++ src/radio/radio.py
@@ -3,11 +3,5 @@ import numpy as np
 
 def getdata():
 
-	radio = pd.read_csv('../../../secret/data/radio_result.csv')
-	print(radio)
-
-	icd10 = pd.read_csv('../../../secret/data/icd10_2010.csv')
-	print(icd10)
-	
-
-getdata()
+	result = pd.read_csv('../../secret/data/radio/radio_icd10.csv')
+	print(result)
