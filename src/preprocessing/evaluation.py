090b770ffcb2360bdb55b500ec1b0421164b39f3
Piyapong <u4507075@hotmail.com>
2019-04-02 08:37:04 +0000

    fix

--- src/preprocessing/evaluation.py
+++ src/preprocessing/evaluation.py
@@ -4,6 +4,7 @@ import numpy as np
 import ntpath
 import os
 import pickle
+import re
 
 def predict_testset():
 
@@ -12,17 +13,21 @@ def predict_testset():
 	for txn in txn_testset:
 		for filename in files:
 			if filename != 'txn_testset.csv':
-				for df in  pd.read_csv(p, chunksize=1000000, index_col=0):
+				print(filename)
+				print(txn)
+				for df in  pd.read_csv('../../secret/data/testset/'+filename, chunksize=1000000, index_col=0):
 					df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-					df = df[df['icd10']==txn]
-					 if len(df) > 0:
-						for row in df.iterrows():
-							print('Label : '+row['icd10'])
+					df = df[df['TXN']==txn]
+					if len(df) > 0:
+						for index,row in df.iterrows():
+							print('### Label : '+str(row['icd10']))
 							if os.path.exists('../../secret/data/model/'+filename.replace('.csv','')):
 								for feature in os.listdir('../../secret/data/model/'+filename.replace('.csv','')):
 									model = pickle.load(open('../../secret/data/model/'+filename.replace('.csv','')+'/'+feature, 'rb'))
-									pre = model.predict(row[:len(row)-1])
-									print(pre)
+									pre = model.predict(row[1:len(row)-1].tolist())
+									if not pre[0].startswith('not'):
+										print(pre)
+										print(model.predict_proba(row[1:len(row)-1].tolist()))
 
 
 
