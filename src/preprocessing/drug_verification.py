8d1bf0a314ef681f6504acd8a8b8edeeeb3bcfc2
Piyapong <u4507075@hotmail.com>
2019-04-04 06:52:51 +0000

    fix

--- src/preprocessing/drug_verification.py
+++ src/preprocessing/drug_verification.py
@@ -8,7 +8,7 @@ dp = '../../secret/data/drug/drug_clean.csv'
 chunk = 100000
 
 def get_drug_verification_registration_data(p):
-	p = '../../secret/data/registration/registration_onehot.csv'
+	#p = '../../secret/data/registration/registration_onehot.csv'
 	for df in  pd.read_csv(p, chunksize=chunk, index_col=0):
 		df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 		for dfp in  pd.read_csv(dp, chunksize=chunk, index_col=0):
