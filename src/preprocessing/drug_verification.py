cdf606d20da5efe601d058e4019143cad2903268
Piyapong <u4507075@hotmail.com>
2019-04-04 06:20:37 +0000

    fix

--- src/preprocessing/drug_verification.py
+++ src/preprocessing/drug_verification.py
@@ -4,17 +4,19 @@ import numpy as np
 import ntpath
 import os
 
-dp = '../../secret/data/drug/drug_numeric.csv'
-chunk = 1000
+dp = '../../secret/data/drug/drug_clean.csv'
+chunk = 100000
 
 def get_drug_verification_registration_data():
-	p = '../../secret/data/registration/registration.csv'
+	p = '../../secret/data/registration/registration_onehot.csv'
 	for df in  pd.read_csv(p, chunksize=chunk, index_col=0):
 		df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 		for dfp in  pd.read_csv(dp, chunksize=chunk, index_col=0):
-			dfp = dfp[dfp['TXN','drug']]
+			dfp = dfp[['TXN','drug']]
 			result = pd.merge(df, dfp, how='inner', on=['TXN'])
-			print(result)
+			if len(result) > 0:
+				result = result.drop_duplicates()
+				print(result)
 
 def get_drug_verification_admission_data():
 	print('x')
