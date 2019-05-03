a713dbccd910168e5cd070bb94735f03fe2c03e8
Piyapong <u4507075@hotmail.com>
2019-05-03 07:29:27 +0000

    fix

--- src/main.py
+++ src/main.py
@@ -69,7 +69,7 @@ import config
 #word_to_vec('adm')
 #word_to_vec('reg')
 #word_to_vec('dru')
-##word_to_vec('idru')
+#word_to_vec('idru')
 #word_to_vec('lab')
 ##word_to_vec('ilab')
 #radio_to_vec('rad')
