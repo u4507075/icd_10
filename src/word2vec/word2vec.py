2c0cf17f03ae23f56639a3dc65ad9d6a77a1a7a7
Piyapong <u4507075@hotmail.com>
2020-08-25 20:18:28 +0700

    word2vec

---
+++ src/word2vec/word2vec.py
@@ -0,0 +1,53 @@
+import mysql.connector as mysql
+import pandas as pd
+from pathlib import Path
+import numpy as np
+import os
+import re
+
+path = '../../../secret/data/'
+
+def chain(max,df,n1,n2):
+        toggle = True
+        d = df.sample(n=1)
+        # d = df.sample(n=1,weights=df[n1+'_weight'])
+        last = d[n1].iloc[0]
+        text = []
+        n = 0
+        for i in range(max):
+                if toggle:
+                        text.append([last, last])
+                        x = [last]
+                        y = [last]
+                        icd10 = df[df[n1] == last].sample(n=1)[n2].iloc[0]
+                        x.insert(0, icd10)
+                        last = df[df[n2] == icd10].sample(n=1)[n1].iloc[0]
+                        y.insert(0, last)
+                        text.append(x)
+                        text.append(y)
+                        last = icd10
+                        toggle = False
+                else:
+                        text.append([last, last])
+                        x = [last]
+                        y = [last]
+                        drug = df[df[n2] == last].sample(n=1)[n1].iloc[0]
+                        x.insert(0, drug)
+                        last = df[df[n1] == drug].sample(n=1)[n2].iloc[0]
+                        y.insert(0, last)
+                        text.append(x)
+                        text.append(y)
+                        last = drug
+                        toggle = True
+
+        return text
+
+def train_chain(filename,f1,f2):
+	df = pd.read_csv(path + 'trainingset/raw/' + filename + '.csv', index_col=0)
+	df = df[[f1,f2]]
+	text = chain(1000, df, f1, f2)
+	print(text)
+
+#reg
+#,txn,sex,age,wt,pulse,resp,temp,blood,rh,room,room_dc,dx_type,sbp,dbp,icd10
+train_chain('reg','sex','icd10')
