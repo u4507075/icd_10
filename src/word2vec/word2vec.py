79a70271491bcb6b61914608ceaa5eb5a7fd4d2c
Piyapong <u4507075@hotmail.com>
2020-08-27 11:34:10 +0700

    function word2vec

--- src/word2vec/word2vec.py
+++ src/word2vec/word2vec.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import numpy as np
 import os
 import re
+import gensim
 
 path = '../../../secret/data/'
 
@@ -43,11 +44,36 @@ def chain(max,df,n1,n2):
         return text
 
 def train_chain(filename,f1,f2):
-	df = pd.read_csv(path + 'trainingset/raw/' + filename + '.csv', index_col=0)
+	modelpath = path+'word2vec_model/'+f1+'_'+f2
+	if not os.path.exists(modelpath):
+		os.makedirs(modelpath)
+	df = pd.read_csv(path + 'trainingset/raw/' + filename + '.csv', index_col=0, low_memory=False)
 	df = df[[f1,f2]]
-	text = chain(1000, df, f1, f2)
-	print(text)
+	t = 1000000
+	p = 100
+	s = 0
+	model = None
+	for i in range(t, -1, -1 * p):
+		file = Path(modelpath + '/' + f1+'_'+f2 + '_' + str(i))
+		if file.is_file():
+			model = gensim.models.Word2Vec.load(modelpath + '/' + f1+'_'+f2 + '_' + str(i))
+			s = i
+			break
+	if model is None:
+		model = gensim.models.Word2Vec(chain(1000, df, f1, f2), compute_loss=True, sg=1)
+	print(s)
+
+	for i in range(s + 1, t):
+		text = chain(100, df, f1, f2)
+		model.build_vocab(text, update=True)
+		model.train(text, total_examples=model.corpus_count, compute_loss=True, epochs=10)
+		print(i)
+		print(model.get_latest_training_loss())
+		if i % p == 0:
+			model.save(modelpath + '/' + f1+'_'+f2 + '_' + str(i))
+			print('saved ' + modelpath + '/' + f1+'_'+f2 + '_' + str(i))
 
 #reg
 #,txn,sex,age,wt,pulse,resp,temp,blood,rh,room,room_dc,dx_type,sbp,dbp,icd10
-train_chain('reg','sex','icd10')
+#train_chain('reg','sex','icd10')
+train_chain('reg','age','icd10')
